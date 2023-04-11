cfe4c7f6deea31b9262c9efe845a9222cb80c347
PedroCost4 <pedro.costacdc@gmail.com>
2023-04-11 16:50:20 -0300

    simpleSorting - DataStructures

---
+++ src/main/java/sorting/variationsOfBubblesort/BidirectionalBubbleSort.java
@@ -0,0 +1,34 @@
+package sorting.variationsOfBubblesort;
+
+import sorting.AbstractSorting;
+import util.Util;
+
+/**
+ * This bubble sort variation has two internal iterations. In the first, it
+ * pushes big elements to the right, like the normal bubble sort does. Then in
+ * the second, iterates the array backwards, that is, from right to left, while
+ * pushing small elements to the left. This process is repeated until the array
+ * is sorted.
+ */
+public class BidirectionalBubbleSort<T extends Comparable<T>> extends
+		AbstractSorting<T> {
+
+	@Override
+	public void sort(T[] array, int leftIndex, int rightIndex) {
+			if (!(leftIndex > rightIndex || leftIndex < 0 || rightIndex > array.length - 1) || array.length == 0) {
+				for (int i = leftIndex; i <= rightIndex; i++) {
+					for (int j = i; j < rightIndex; j++) {
+						if (array[j].compareTo(array[j + 1]) > 0) {
+							Util.swap(array, j, j + 1);
+						}
+						}
+					for (int k = rightIndex; k > i; k--) {
+						if (array[k - 1].compareTo(array[k]) > 0) {
+							Util.swap(array, k, k - 1);
+						}
+						}
+					rightIndex--;
+				}
+			}
+	}
+}
